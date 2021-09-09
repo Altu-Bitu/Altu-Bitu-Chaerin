bcde8980950aa85edd5ecf3ce4da40a06ad8072f
chaerin <hikimrin@gmail.com>
2021-09-09 16:21:47 +0900

    [정렬] 9월 9일 - ing

---
+++ 9월 3일 - 정렬/cmake-build-debug/CMakeFiles/9_3___.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "9_3___"
+  "9_3___.pdb"
+  "CMakeFiles/9_3___.dir/2750.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9_3___.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
